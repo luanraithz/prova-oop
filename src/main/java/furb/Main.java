9f12f8d5b14e88632ce4c6d255aa89885dc6dd9c
Luan Raithz Machado <luanraithz@gmail.com>
2020-08-19 21:32:30 -0300

    Removed wrong code

--- src/main/java/furb/Main.java
+++ src/main/java/furb/Main.java
@@ -9,8 +9,6 @@ import java.io.FileInputStream;
 public class Main  {
 
     public static void main(String[] args) {
-        FileInputStream file = new FileInputStream("save");
-        file.
         Empresa empresa = new Empresa();
         Home home = new Home(empresa);
         home.setLocationRelativeTo(null);
